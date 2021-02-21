a3609f0755244fed109e208290ccd00392ca53a6
Luke Renaud <luke@dabblee.com>
2021-02-20 19:10:52 -0800

    Hide errors, and reduce arch space.

--- snap/local/sh/symlink-configs.sh
+++ snap/local/sh/symlink-configs.sh
@@ -7,14 +7,14 @@ set \
 	-o pipefail
 
 # These are kind of arbitrary. The first two are typical
-ln -s $SNAP_REAL_HOME/.pokerc $HOME/.pokerc || true
-ln -s $SNAP_REAL_HOME/.poke_history $HOME/.poke_history || true
+ln -s $SNAP_REAL_HOME/.pokerc $HOME/.pokerc 2>/dev/null || true
+ln -s $SNAP_REAL_HOME/.poke_history $HOME/.poke_history 2>/dev/null || true
 
 # These two are what I expect most users will end up wanting, but I don't have
 # any firm justification, and snap confinement means I have to be very explicit
 # in allowing them.
-ln -s $SNAP_REAL_HOME/.poke.d $HOME/.poke.d || true
+ln -s $SNAP_REAL_HOME/.poke.d $HOME/.poke.d 2>/dev/null || true
 mkdir -p $HOME/.config
-ln -s $SNAP_REAL_HOME/.config/poke  $HOME/.config/poke || true
+ln -s $SNAP_REAL_HOME/.config/poke  $HOME/.config/poke 2>/dev/null || true
 
 exec "${@}"
